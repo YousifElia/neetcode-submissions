029ecb53d43bef9017440a4621263b48e9efcf3c
Yousif Elia <yousif.eliaa@gmail.com>
2026-06-08 21:30:06 -0400

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        int[] char_counts = new int[26]; // 26 letters in the alpha.
+        for(int i = 0; i < s.length(); i++) {
+            char_counts[s.charAt(i)-'a']++;
+            char_counts[t.charAt(i)-'a']--;
+        }
+        for(int count : char_counts) {
+            if(count != 0) return false;
+        } return true;
+    }
+}

64ad33b4c6ce46e7d84bec1b07e04631e90cc4f0
Yousif Elia <yousif.eliaa@gmail.com>
2026-09-11 10:40:09 -0400

    Add: search-2d-matrix - submission-0

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-0.py
@@ -0,0 +1,23 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        row, column = len(matrix), len(matrix[0])
+        max = row * column
+
+        for i in range(row):
+            for j in range(column):
+                if matrix[i][j] == target:
+                    return True
+        return False
+        
+
+
+# Sorted 2D Array
+# Find the Middle 
+# low, high = 0, len(arr) - 1
+#  len(List[List[int]]) / 2
+#
+#  if List[List[int]] > target:
+#       List[List[int]] / 4
+#  elif List[List[int]] < target:
+#       List[List[int]] * 2
+#  else return
